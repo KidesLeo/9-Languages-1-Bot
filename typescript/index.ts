3ca3aab5bca7617da780fdc6ef751402c2e2f2d5
kidusleo <kidusleo@gmail.com>
2023-05-15 23:12:30 +0300

    Added generateString function

---
+++ typescript/index.ts
@@ -0,0 +1,17 @@
+import { Telegraf } from "telegraf";
+import * as dotenv from "dotenv";
+
+function generateString(length: number) {
+    const KeyString =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=";
+
+    let RandString = "";
+
+    for (let i = 0; i < length; i++) {
+        RandString += KeyString.charAt(
+            Math.floor(Math.random() * KeyString.length)
+        );
+    }
+
+    return RandString;
+}
